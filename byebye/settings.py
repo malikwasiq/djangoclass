97803d0bbd46ee4563fac3e324dff21ecc6fa7a4
Kashaf Ansari <kashafansari3108@gmail.com>
2022-02-06 15:34:11 +0500

    host

--- byebye/settings.py
+++ byebye/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 import os 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = 'static/'
 ROOT_URLCONF = 'byebye.urls'
 
 TEMPLATES = [
@@ -121,8 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-import os 
-STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
